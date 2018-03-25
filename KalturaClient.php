4b1f5305d877e8bddc331cbf60f45c56c78e0ba5
Jenkins <jenkins@kaltura.com>
2018-03-25 00:00:08 +0000

    Auto-generated by Jenkins

--- KalturaClient.php
+++ KalturaClient.php
@@ -7602,8 +7602,8 @@ class KalturaClient extends KalturaClientBase
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:18-03-23');
-		$this->setApiVersion('4.72.71.43139');
+		$this->setClientTag('php5:18-03-24');
+		$this->setApiVersion('4.72.71.43145');
 		
 		$this->announcement = new KalturaAnnouncementService($this);
 		$this->appToken = new KalturaAppTokenService($this);
