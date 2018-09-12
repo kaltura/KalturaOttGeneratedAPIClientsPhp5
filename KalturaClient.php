959d099299261c4c544c1454f3f81d1997203632
Jenkins <jenkins@kaltura.com>
2018-09-12 06:53:44 +0000

    Auto-generated by Jenkins

--- KalturaClient.php
+++ KalturaClient.php
@@ -9497,8 +9497,8 @@ class KalturaClient extends KalturaClientBase
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:18-09-11');
-		$this->setApiVersion('5.0.2.43149');
+		$this->setClientTag('php5:18-09-12');
+		$this->setApiVersion('5.0.2.12383');
 		
 		$this->announcement = new KalturaAnnouncementService($this);
 		$this->appToken = new KalturaAppTokenService($this);
--- README.txt
+++ README.txt
@@ -1,2 +1,2 @@
 Kaltura PHP 5 API Client Library.
-Compatible with Kaltura server version 5.0.2.43149 and above.
+Compatible with Kaltura server version 5.0.2.12383 and above.
