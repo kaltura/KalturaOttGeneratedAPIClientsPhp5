1f35445cf6c258aebe6c0d1c694f9c69c706c80f
Jenkins <jenkins@kaltura.com>
2019-01-06 10:15:21 +0000

    Auto-generated by Jenkins

--- KalturaTypes.php
+++ KalturaTypes.php
@@ -9621,7 +9621,7 @@ class KalturaClientConfiguration extends KalturaObjectBase
 	/**
 	 * Abort the Multireuqset call if any error occurs in one of the requests
 	 *
-	 * @var string
+	 * @var bool
 	 */
 	public $abortOnError = null;
 
