95b8aa00d5694ca448e20aea1d0a1b887a8e1f87
Jenkins <jenkins@kaltura.com>
2019-01-23 14:24:52 +0000

    Auto-generated by Jenkins

--- KalturaClient.php
+++ KalturaClient.php
@@ -9,7 +9,7 @@
 // to do with audio, video, and animation what Wiki platfroms allow them to do with
 // text.
 //
-// Copyright (C) 2006-2018  Kaltura Inc.
+// Copyright (C) 2006-2019  Kaltura Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -9499,8 +9499,8 @@ class KalturaClient extends KalturaClientBase
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:18-11-22');
-		$this->setApiVersion('5.0.2.17073');
+		$this->setClientTag('php5:19-01-23');
+		$this->setApiVersion('5.0.2.25914');
 		
 		$this->announcement = new KalturaAnnouncementService($this);
 		$this->appToken = new KalturaAppTokenService($this);
--- README.txt
+++ README.txt
@@ -1,2 +1,2 @@
 Kaltura PHP 5 API Client Library.
-Compatible with Kaltura server version 5.0.2.17073 and above.
+Compatible with Kaltura server version 5.0.2.25914 and above.
