0852ce3ee01afd76d8bc153e92aee3aab30c97e0
Backend CI <ott.rnd.core@kaltura.com>
2020-01-13 08:30:01 +0000

    Auto-generated by Jenkins

--- KalturaClient.php
+++ KalturaClient.php
@@ -10812,8 +10812,8 @@ class KalturaClient extends KalturaClientBase
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:20-01-08');
-		$this->setApiVersion('5.2.8.14040');
+		$this->setClientTag('php5:20-01-13');
+		$this->setApiVersion('5.2.8.14043');
 		
 		$this->announcement = new KalturaAnnouncementService($this);
 		$this->appToken = new KalturaAppTokenService($this);
