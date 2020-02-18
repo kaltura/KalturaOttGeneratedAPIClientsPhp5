0904e52861e361d78edf76761c3f0076d20e9cae
Backend CI <ott.rnd.core@kaltura.com>
2020-02-18 11:34:25 +0000

    Auto-generated by Jenkins job OTT-BE-Phoenix-Windows, build 2525, branch 531_BEO-7569_TVM_Export_EPG_kSql_filter_dates_failed

--- KalturaClient.php
+++ KalturaClient.php
@@ -10988,7 +10988,7 @@ class KalturaClient extends KalturaClientBase
 		parent::__construct($config);
 		
 		$this->setClientTag('php5:20-02-18');
-		$this->setApiVersion('5.3.2.14683');
+		$this->setApiVersion('5.3.2.14673');
 		
 		$this->announcement = new KalturaAnnouncementService($this);
 		$this->appToken = new KalturaAppTokenService($this);
