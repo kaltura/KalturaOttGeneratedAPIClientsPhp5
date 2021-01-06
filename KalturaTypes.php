217ce788fc8c2a6313ef407a0f48b24b6fa4a98c
Backend CI <ott.rnd.core@kaltura.com>
2021-01-06 01:00:32 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/335, branch master

--- KalturaTypes.php
+++ KalturaTypes.php
@@ -9,7 +9,7 @@
 // to do with audio, video, and animation what Wiki platfroms allow them to do with
 // text.
 //
-// Copyright (C) 2006-2020  Kaltura Inc.
+// Copyright (C) 2006-2021  Kaltura Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -3951,6 +3951,13 @@ class KalturaMediaImage extends KalturaObjectBase
 	 * @var bool
 	 */
 	public $isDefault = null;
+
+	/**
+	 * Image type identifier
+	 *
+	 * @var int
+	 */
+	public $imageTypeId = null;
 
 
 }
@@ -9413,6 +9420,13 @@ class KalturaConcurrencyPartnerConfig extends KalturaPartnerConfiguration
 	 * @var int
 	 */
 	public $concurrencyThresholdInSeconds = null;
+
+	/**
+	 * Revoke on device delete
+	 *
+	 * @var bool
+	 */
+	public $revokeOnDeviceDelete = null;
 
 
 }
