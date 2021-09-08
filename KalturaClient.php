27030d7e994255e32caca0c3e36d3ab372d8b09b
Backend CI <ott.rnd.core@kaltura.com>
2021-09-08 14:21:43 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/617, branch 6_7_1

--- KalturaClient.php
+++ KalturaClient.php
@@ -6,7 +6,7 @@
 //                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
 //
 // This file is part of the Kaltura Collaborative Media Suite which allows users
-// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// to do with audio, video, and animation what Wiki platforms allow them to do with
 // text.
 //
 // Copyright (C) 2006-2021  Kaltura Inc.
@@ -13110,8 +13110,8 @@ class KalturaClient extends KalturaClientBase
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:21-09-02');
-		$this->setApiVersion('6.7.0.29271');
+		$this->setClientTag('php5:21-09-08');
+		$this->setApiVersion('6.7.0.29274');
 		
 		$this->announcement = new KalturaAnnouncementService($this);
 		$this->appToken = new KalturaAppTokenService($this);
