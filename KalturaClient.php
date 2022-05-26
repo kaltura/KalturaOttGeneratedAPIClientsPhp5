22e3c105bbd268d7f4de286483902427cc2feb1f
Backend CI <ott.rnd.core@kaltura.com>
2022-05-26 11:20:25 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/904, branch 7_6_0

--- KalturaClient.php
+++ KalturaClient.php
@@ -13976,8 +13976,8 @@ class KalturaClient extends KalturaClientBase
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:22-05-19');
-		$this->setApiVersion('7.6.0.29894');
+		$this->setClientTag('php5:22-05-26');
+		$this->setApiVersion('7.6.0.29896');
 		
 		$this->announcement = new KalturaAnnouncementService($this);
 		$this->appToken = new KalturaAppTokenService($this);
