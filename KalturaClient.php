9404f78f45376a895ec2420be5d2ef04183506ae
Backend CI <ott.rnd.core@kaltura.com>
2022-07-19 14:19:18 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/934, branch BEO-12422_device_brands_and_families_in_asw

--- KalturaClient.php
+++ KalturaClient.php
@@ -14183,8 +14183,8 @@ class KalturaClient extends KalturaClientBase
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:22-06-21');
-		$this->setApiVersion('7.7.0.29917');
+		$this->setClientTag('php5:22-07-19');
+		$this->setApiVersion('7.8.1.29970');
 		
 		$this->announcement = new KalturaAnnouncementService($this);
 		$this->appToken = new KalturaAppTokenService($this);
