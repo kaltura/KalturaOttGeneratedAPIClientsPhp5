1897527fc8b116de117a538e39fe869f05c3f939
Backend CI <ott.rnd.core@kaltura.com>
2022-11-06 19:22:04 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/985, branch 8_2_0

--- KalturaTypes.php
+++ KalturaTypes.php
@@ -700,6 +700,13 @@ class KalturaSegmentationTypeFilter extends KalturaBaseSegmentationTypeFilter
 	 */
 	public $kSql = null;
 
+	/**
+	 * Name of segment contains specific string value
+	 *
+	 * @var string
+	 */
+	public $nameContain = null;
+
 
 }
 
@@ -3135,6 +3142,7 @@ class KalturaBookmark extends KalturaSlimAsset
 
 	/**
 	 * The position of the user in the specific asset (in seconds)
+	 *             For external recordings will always be &#39;0&#39;
 	 *
 	 * @var int
 	 * @insertonly
@@ -3151,6 +3159,7 @@ class KalturaBookmark extends KalturaSlimAsset
 
 	/**
 	 * Specifies whether the user&#39;s current position exceeded 95% of the duration
+	 *             For external recordings will always be &#39;True&#39;
 	 *
 	 * @var bool
 	 * @readonly
@@ -3781,6 +3790,22 @@ class KalturaCampaignIdInFilter extends KalturaCampaignFilter
 	 * @var string
 	 */
 	public $idIn = null;
+
+
+}
+
+/**
+ * @package Kaltura
+ * @subpackage Client
+ */
+class KalturaCampaignSegmentFilter extends KalturaCampaignSearchFilter
+{
+	/**
+	 * segment id to be searched inside campaigns
+	 *
+	 * @var int
+	 */
+	public $segmentIdEqual = null;
 
 
 }
@@ -4986,7 +5011,6 @@ class KalturaUsageModule extends KalturaObjectBase
 	 * The amount time an item is available for viewing since a user started watching the item
 	 *
 	 * @var int
-	 * @insertonly
 	 */
 	public $viewLifeCycle = null;
 
@@ -4994,7 +5018,6 @@ class KalturaUsageModule extends KalturaObjectBase
 	 * The amount time an item is available for viewing
 	 *
 	 * @var int
-	 * @insertonly
 	 */
 	public $fullLifeCycle = null;
 
@@ -10148,6 +10171,13 @@ class KalturaSegmentationType extends KalturaObjectBase
 	public $conditions;
 
 	/**
+	 * Boolean operator between segmentation type&#39;s conditions - defaults to &quot;And&quot;
+	 *
+	 * @var KalturaBooleanOperator
+	 */
+	public $conditionsOperator = null;
+
+	/**
 	 * Segmentation conditions - can be empty
 	 *
 	 * @var array of KalturaBaseSegmentAction
@@ -10168,6 +10198,22 @@ class KalturaSegmentationType extends KalturaObjectBase
 	 * @readonly
 	 */
 	public $createDate = null;
+
+	/**
+	 * Update date of segmentation type
+	 *
+	 * @var int
+	 * @readonly
+	 */
+	public $updateDate = null;
+
+	/**
+	 * Last date of execution of segmentation type
+	 *
+	 * @var int
+	 * @readonly
+	 */
+	public $executeDate = null;
 
 	/**
 	 * Segmentation type version
@@ -10332,6 +10378,13 @@ class KalturaMonetizationCondition extends KalturaBaseSegmentCondition
 	 * @var string
 	 */
 	public $businessModuleIdIn = null;
+
+	/**
+	 * Which currency code should be taken into consideration
+	 *
+	 * @var string
+	 */
+	public $currencyCode = null;
 
 
 }
@@ -10719,6 +10772,14 @@ class KalturaSingleSegmentValue extends KalturaBaseSegmentValue
 	 * @readonly
 	 */
 	public $affectedUsers = null;
+
+	/**
+	 * The amount of households that are being affected by this Segmentation type
+	 *
+	 * @var int
+	 * @readonly
+	 */
+	public $affectedHouseholds = null;
 
 
 }
@@ -20206,6 +20267,85 @@ class KalturaAssetStatisticsQuery extends KalturaObjectBase
  * @package Kaltura
  * @subpackage Client
  */
+class KalturaBulkUploadStatistics extends KalturaObjectBase
+{
+	/**
+	 * count of bulk upload in pending status
+	 *
+	 * @var int
+	 */
+	public $pending = null;
+
+	/**
+	 * count of bulk Uploaded in uploaded status
+	 *
+	 * @var int
+	 */
+	public $uploaded = null;
+
+	/**
+	 * count of bulk upload in queued status
+	 *
+	 * @var int
+	 */
+	public $queued = null;
+
+	/**
+	 * count of bulk upload in parsing status
+	 *
+	 * @var int
+	 */
+	public $parsing = null;
+
+	/**
+	 * count of bulk upload in processing status
+	 *
+	 * @var int
+	 */
+	public $processing = null;
+
+	/**
+	 * count of bulk upload in processed status
+	 *
+	 * @var int
+	 */
+	public $processed = null;
+
+	/**
+	 * count of bulk upload in success status
+	 *
+	 * @var int
+	 */
+	public $success = null;
+
+	/**
+	 * count of bulk upload in partial status
+	 *
+	 * @var int
+	 */
+	public $partial = null;
+
+	/**
+	 * count of bulk upload in failed status
+	 *
+	 * @var int
+	 */
+	public $failed = null;
+
+	/**
+	 * count of bulk upload in fatal status
+	 *
+	 * @var int
+	 */
+	public $fatal = null;
+
+
+}
+
+/**
+ * @package Kaltura
+ * @subpackage Client
+ */
 class KalturaOTTCategory extends KalturaObjectBase
 {
 	/**
@@ -21226,7 +21366,7 @@ class KalturaIotClientConfiguration extends KalturaObjectBase
 	/**
 	 * topics
 	 *
-	 * @var array of KalturaStringValue
+	 * @var array of KalturaKeyValue
 	 */
 	public $topics;
 
@@ -22077,6 +22217,29 @@ class KalturaSearchPriorityGroupOrderedIdsSet extends KalturaObjectBase
 	 * @var string
 	 */
 	public $priorityGroupIds = null;
+
+
+}
+
+/**
+ * @package Kaltura
+ * @subpackage Client
+ */
+class KalturaSegmentationPartnerConfiguration extends KalturaObjectBase
+{
+	/**
+	 * The maximum number of past days to be calculated for dynamic segments, default=180
+	 *
+	 * @var int
+	 */
+	public $maxCalculatedPeriod = null;
+
+	/**
+	 * How many dynamic segments (segments with conditions) the operator is allowed to have, default=50
+	 *
+	 * @var int
+	 */
+	public $maxDynamicSegments = null;
 
 
 }
