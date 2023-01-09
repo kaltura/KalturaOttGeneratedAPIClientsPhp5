a25fbf48196dfda122c4b09d7e7034a26d3e2ce2
Backend CI <ott.rnd.core@kaltura.com>
2023-01-09 14:14:26 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1029, branch BEO-13063_add_recording

--- KalturaClient.php
+++ KalturaClient.php
@@ -9978,16 +9978,14 @@ class KalturaRecordingService extends KalturaServiceBase
 	 * @param bigint $assetId Asset identifier
 	 * @param bigint $epgChannelId Epg channel identifier
 	 * @param int $endPadding End padding offset
-	 * @param string $crid Crid
 	 * @return KalturaImmediateRecording
 	 */
-	function immediateRecord($assetId, $epgChannelId, $endPadding, $crid = null)
+	function immediateRecord($assetId, $epgChannelId, $endPadding)
 	{
 		$kparams = array();
 		$this->client->addParam($kparams, "assetId", $assetId);
 		$this->client->addParam($kparams, "epgChannelId", $epgChannelId);
 		$this->client->addParam($kparams, "endPadding", $endPadding);
-		$this->client->addParam($kparams, "crid", $crid);
 		$this->client->queueServiceActionCall("recording", "immediateRecord", $kparams);
 		if ($this->client->isMultiRequest())
 			return $this->client->getMultiRequestResult();
@@ -14325,7 +14323,7 @@ class KalturaClient extends KalturaClientBase
 		parent::__construct($config);
 		
 		$this->setClientTag('php5:23-01-09');
-		$this->setApiVersion('8.4.3.30334');
+		$this->setApiVersion('8.4.3.30335');
 		
 		$this->announcement = new KalturaAnnouncementService($this);
 		$this->appToken = new KalturaAppTokenService($this);
