fd8cb60251c3d2e5e42b04e3e22a0a05780b40f8
Backend CI <ott.rnd.core@kaltura.com>
2024-01-15 15:26:10 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1271, branch BEO-14417_add-rsp-strategies

--- KalturaTypes.php
+++ KalturaTypes.php
@@ -9652,6 +9652,13 @@ class KalturaSSOAdapterProfile extends KalturaObjectBase
 	 */
 	public $sharedSecret = null;
 
+	/**
+	 * Adapter GRPC Address, without protocol, i.e: &#39;adapter-hostname:9090&#39;
+	 *
+	 * @var string
+	 */
+	public $adapterGrpcAddress = null;
+
 
 }
 
@@ -12167,6 +12174,47 @@ class KalturaCatalogPartnerConfig extends KalturaPartnerConfiguration
  * @package Kaltura
  * @subpackage Client
  */
+class KalturaCloudUploadSettingsConfiguration extends KalturaPartnerConfiguration
+{
+	/**
+	 * Comma seperated list of file extensions that allowed to all partners
+	 *
+	 * @var string
+	 * @readonly
+	 */
+	public $defaultAllowedFileExtensions = null;
+
+	/**
+	 * Comma seperated list of file extensions that allowed to partner in question
+	 *             {&quot;jpeg&quot;,&quot;image/jpeg&quot;},
+	 *             {&quot;jpg&quot;,&quot;image/jpeg&quot;},
+	 *             {&quot;png&quot;,&quot;image/png&quot;},
+	 *             {&quot;tif&quot;,&quot;image/tiff&quot;},
+	 *             {&quot;tiff&quot;,&quot;image/tiff&quot;},
+	 *             {&quot;gif&quot;,&quot;image/gif&quot;},
+	 *             {&quot;xls&quot;,&quot;application/vnd.ms-excel&quot;},
+	 *             {&quot;xlsx&quot;,&quot;application/vnd.openxmlformats-officedocument.spreadsheetml.sheet&quot;},
+	 *             {&quot;csv&quot;,&quot;text/csv&quot;},
+	 *             {&quot;xml&quot;,&quot;text/xml&quot;},
+	 *             {&quot;txt&quot;,&quot;text/plain&quot;},
+	 *             {&quot;doc&quot;,&quot;application/msword&quot;},
+	 *             {&quot;docx&quot;,&quot;application/vnd.openxmlformats-officedocument.wordprocessingml.document&quot;},
+	 *             {&quot;bmp&quot;,&quot;image/bmp&quot;},
+	 *             {&quot;ico&quot;,&quot;image/x-icon&quot;},
+	 *             {&quot;mp3&quot;,&quot;audio/mpeg&quot;},
+	 *             {&quot;pdf&quot;,&quot;application/pdf&quot;}}
+	 *
+	 * @var string
+	 */
+	public $customAllowedFileExtensions = null;
+
+
+}
+
+/**
+ * @package Kaltura
+ * @subpackage Client
+ */
 class KalturaBookmarkEventThreshold extends KalturaObjectBase
 {
 	/**
@@ -12427,7 +12475,7 @@ class KalturaGeneralPartnerConfig extends KalturaPartnerConfiguration
 	public $dateFormat = null;
 
 	/**
-	 * Household limitation&#160;module
+	 * Household limitation module
 	 *
 	 * @var int
 	 */
@@ -15000,6 +15048,10 @@ class KalturaBillingTransaction extends KalturaObjectBase
 
 	/**
 	 * Billing Action
+	 *             Note: when purchasing subscription that is “ENTITLED_TO_PREVIEW_MODULE”:
+	 *              the first BillingTransaction.billingAction will be “unknown”, 
+	 *              the second BillingTransaction.billingAction will be “purchase”, 
+	 *              and the rest of them will be “renew_payment&quot;.
 	 *
 	 * @var KalturaBillingAction
 	 * @readonly
@@ -20324,7 +20376,39 @@ abstract class KalturaRepresentativeSelectionPolicy extends KalturaObjectBase
  * @package Kaltura
  * @subpackage Client
  */
+class KalturaTopEntitledOrFreeRsp extends KalturaRepresentativeSelectionPolicy
+{
+	/**
+	 * order by
+	 *
+	 * @var KalturaBaseAssetOrder
+	 */
+	public $orderBy;
+
+
+}
+
+/**
+ * @package Kaltura
+ * @subpackage Client
+ */
 class KalturaTopRsp extends KalturaRepresentativeSelectionPolicy
+{
+	/**
+	 * order by
+	 *
+	 * @var KalturaBaseAssetOrder
+	 */
+	public $orderBy;
+
+
+}
+
+/**
+ * @package Kaltura
+ * @subpackage Client
+ */
+class KalturaTopSubscriptionEntitledOrFreeRsp extends KalturaRepresentativeSelectionPolicy
 {
 	/**
 	 * order by
@@ -21202,6 +21286,29 @@ class KalturaCompensation extends KalturaObjectBase
  * @package Kaltura
  * @subpackage Client
  */
+class KalturaCouponFilesLinks extends KalturaObjectBase
+{
+	/**
+	 * Total count of coupons code files
+	 *
+	 * @var int
+	 */
+	public $totalCount = null;
+
+	/**
+	 * A pre-signed URL pointing to a coupon codes file
+	 *
+	 * @var array of KalturaStringValue
+	 */
+	public $objects;
+
+
+}
+
+/**
+ * @package Kaltura
+ * @subpackage Client
+ */
 class KalturaCouponGenerationOptions extends KalturaObjectBase
 {
 
@@ -21668,6 +21775,45 @@ class KalturaTriggerCampaignEvent extends KalturaEventObject
 	 * @readonly
 	 */
 	public $householdId = null;
+
+
+}
+
+/**
+ * @package Kaltura
+ * @subpackage Client
+ */
+class KalturaRetryDeleteRequest extends KalturaObjectBase
+{
+	/**
+	 * The first date (epoch) to start the retryDelete from - by default {now} - {30 days in second}
+	 *
+	 * @var int
+	 */
+	public $startDate = null;
+
+	/**
+	 * The last date (epoch) to do the retryDelete - by default {now} (should be greater than startDate)
+	 *
+	 * @var int
+	 */
+	public $endDate = null;
+
+
+}
+
+/**
+ * @package Kaltura
+ * @subpackage Client
+ */
+class KalturaHouseholdPartnerConfiguration extends KalturaObjectBase
+{
+	/**
+	 * Retention period in days.
+	 *
+	 * @var int
+	 */
+	public $retentionPeriodDays = null;
 
 
 }
