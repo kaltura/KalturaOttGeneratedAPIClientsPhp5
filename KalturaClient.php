8da2004e5063817e44c10a21b9822d3424dc01b3
Backend CI <ott.rnd.core@kaltura.com>
2024-10-30 15:37:05 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1359, branch 10.6.0.1

--- KalturaClient.php
+++ KalturaClient.php
@@ -352,7 +352,7 @@ class KalturaAssetService extends KalturaServiceBase
 	}
 
 	/**
-	 * Add new bulk upload batch job Conversion profile id can be specified in the API.
+	 * Add new bulk upload batch job Conversion profile id can be specified in the API (note that the total request body size is limited to 10MB).
 	 * 
 	 * @param file $fileData FileData
 	 * @param KalturaBulkUploadJobData $bulkUploadJobData BulkUploadJobData
@@ -1078,7 +1078,8 @@ class KalturaAssetStatisticsService extends KalturaServiceBase
 	}
 
 	/**
-	 * Returns statistics for given list of assets by type and / or time period
+	 * Returns statistics for given list of assets by type and / or time period.
+            Supported values for KalturaAssetStatisticsQuery.assetTypeEqual : KalturaAssetType.media, KalturaAssetType.epg.
 	 * 
 	 * @param KalturaAssetStatisticsQuery $query Query for assets statistics
 	 * @return KalturaAssetStatisticsListResponse
@@ -1890,13 +1891,15 @@ class KalturaCategoryTreeService extends KalturaServiceBase
 	 * 
 	 * @param bigint $versionId Category version id of tree
 	 * @param int $deviceFamilyId DeviceFamilyId related to category tree
+	 * @param bool $filter Filter=true excludes items for which the start date has not been reached yet; default is false
 	 * @return KalturaCategoryTree
 	 */
-	function getByVersion($versionId = null, $deviceFamilyId = null)
+	function getByVersion($versionId = null, $deviceFamilyId = null, $filter = false)
 	{
 		$kparams = array();
 		$this->client->addParam($kparams, "versionId", $versionId);
 		$this->client->addParam($kparams, "deviceFamilyId", $deviceFamilyId);
+		$this->client->addParam($kparams, "filter", $filter);
 		$this->client->queueServiceActionCall("categorytree", "getByVersion", $kparams);
 		if ($this->client->isMultiRequest())
 			return $this->client->getMultiRequestResult();
@@ -3696,7 +3699,7 @@ class KalturaDynamicListService extends KalturaServiceBase
 	}
 
 	/**
-	 * Add new bulk upload batch job Conversion profile id can be specified in the API.
+	 * Add new bulk upload batch job Conversion profile id can be specified in the API (note that the total request body size is limited to 10MB).
 	 * 
 	 * @param file $fileData FileData
 	 * @param KalturaBulkUploadExcelJobData $jobData JobData
@@ -6795,6 +6798,23 @@ class KalturaLineupService extends KalturaServiceBase
 	}
 
 	/**
+	 * Sends lineup requested invalidation
+	 * 
+	 * @return bool
+	 */
+	function invalidate()
+	{
+		$kparams = array();
+		$this->client->queueServiceActionCall("lineup", "invalidate", $kparams);
+		if ($this->client->isMultiRequest())
+			return $this->client->getMultiRequestResult();
+		$resultObject = $this->client->doQueue();
+		$this->client->throwExceptionIfError($resultObject);
+		$resultObject = (bool) $resultObject;
+		return $resultObject;
+	}
+
+	/**
 	 * Returns list of lineup regional linear channels associated with one LCN and its region information. Allows to apply sorting and filtering by LCN and linear channels.
 	 * 
 	 * @param KalturaLineupRegionalChannelFilter $filter Request filter
@@ -7358,6 +7378,54 @@ class KalturaMetaService extends KalturaServiceBase
 		$resultObject = $this->client->doQueue();
 		$this->client->throwExceptionIfError($resultObject);
 		$this->client->validateObjectType($resultObject, "KalturaMeta");
+		return $resultObject;
+	}
+}
+
+/**
+ * @package Kaltura
+ * @subpackage Client
+ */
+class KalturaMfaPartnerConfigurationService extends KalturaServiceBase
+{
+	function __construct(KalturaClient $client = null)
+	{
+		parent::__construct($client);
+	}
+
+	/**
+	 * Get MFA partner configuration.
+	 * 
+	 * @return KalturaMultifactorAuthenticationPartnerConfiguration
+	 */
+	function get()
+	{
+		$kparams = array();
+		$this->client->queueServiceActionCall("mfapartnerconfiguration", "get", $kparams);
+		if ($this->client->isMultiRequest())
+			return $this->client->getMultiRequestResult();
+		$resultObject = $this->client->doQueue();
+		$this->client->throwExceptionIfError($resultObject);
+		$this->client->validateObjectType($resultObject, "KalturaMultifactorAuthenticationPartnerConfiguration");
+		return $resultObject;
+	}
+
+	/**
+	 * Update MFA partner configuration.
+	 * 
+	 * @param KalturaMultifactorAuthenticationPartnerConfiguration $configuration MFA configuration
+	 * @return KalturaMultifactorAuthenticationPartnerConfiguration
+	 */
+	function update(KalturaMultifactorAuthenticationPartnerConfiguration $configuration)
+	{
+		$kparams = array();
+		$this->client->addParam($kparams, "configuration", $configuration->toParams());
+		$this->client->queueServiceActionCall("mfapartnerconfiguration", "update", $kparams);
+		if ($this->client->isMultiRequest())
+			return $this->client->getMultiRequestResult();
+		$resultObject = $this->client->doQueue();
+		$this->client->throwExceptionIfError($resultObject);
+		$this->client->validateObjectType($resultObject, "KalturaMultifactorAuthenticationPartnerConfiguration");
 		return $resultObject;
 	}
 }
@@ -7976,6 +8044,36 @@ class KalturaOttUserService extends KalturaServiceBase
 	}
 
 	/**
+	 * Login based on MFA token.
+	 * 
+	 * @param int $partnerId Partner identifier
+	 * @param string $token MFA token
+	 * @param string $username User name
+	 * @param string $password Password
+	 * @param map $extraParams Extra params
+	 * @param string $udid Device UDID
+	 * @return KalturaLoginResponse
+	 */
+	function mfaLogin($partnerId, $token, $username = null, $password = null, array $extraParams = null, $udid = null)
+	{
+		$kparams = array();
+		$this->client->addParam($kparams, "partnerId", $partnerId);
+		$this->client->addParam($kparams, "token", $token);
+		$this->client->addParam($kparams, "username", $username);
+		$this->client->addParam($kparams, "password", $password);
+		if ($extraParams !== null)
+			$this->client->addParam($kparams, "extraParams", $extraParams->toParams());
+		$this->client->addParam($kparams, "udid", $udid);
+		$this->client->queueServiceActionCall("ottuser", "mfaLogin", $kparams);
+		if ($this->client->isMultiRequest())
+			return $this->client->getMultiRequestResult();
+		$resultObject = $this->client->doQueue();
+		$this->client->throwExceptionIfError($resultObject);
+		$this->client->validateObjectType($resultObject, "KalturaLoginResponse");
+		return $resultObject;
+	}
+
+	/**
 	 * Sign up a new user.
 	 * 
 	 * @param int $partnerId Partner identifier
@@ -8016,6 +8114,32 @@ class KalturaOttUserService extends KalturaServiceBase
 		$resultObject = $this->client->doQueue();
 		$this->client->throwExceptionIfError($resultObject);
 		$resultObject = (bool) $resultObject;
+		return $resultObject;
+	}
+
+	/**
+	 * Resend MFA Token for the user.
+	 * 
+	 * @param int $partnerId Partner identifier
+	 * @param string $username User name
+	 * @param string $password Password
+	 * @param map $extraParams Extra params
+	 * @return KalturaResendMfaTokenResponse
+	 */
+	function resendMfaToken($partnerId, $username = null, $password = null, array $extraParams = null)
+	{
+		$kparams = array();
+		$this->client->addParam($kparams, "partnerId", $partnerId);
+		$this->client->addParam($kparams, "username", $username);
+		$this->client->addParam($kparams, "password", $password);
+		if ($extraParams !== null)
+			$this->client->addParam($kparams, "extraParams", $extraParams->toParams());
+		$this->client->queueServiceActionCall("ottuser", "resendMfaToken", $kparams);
+		if ($this->client->isMultiRequest())
+			return $this->client->getMultiRequestResult();
+		$resultObject = $this->client->doQueue();
+		$this->client->throwExceptionIfError($resultObject);
+		$this->client->validateObjectType($resultObject, "KalturaResendMfaTokenResponse");
 		return $resultObject;
 	}
 
@@ -14194,6 +14318,12 @@ class KalturaClient extends KalturaClientBase
 
 	/**
 	 * 
+	 * @var KalturaMfaPartnerConfigurationService
+	 */
+	public $mfaPartnerConfiguration = null;
+
+	/**
+	 * 
 	 * @var KalturaNotificationService
 	 */
 	public $notification = null;
@@ -14609,8 +14739,8 @@ class KalturaClient extends KalturaClientBase
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:24-01-15');
-		$this->setApiVersion('9.6.0.0');
+		$this->setClientTag('php5:24-10-30');
+		$this->setApiVersion('10.6.0.1');
 		
 		$this->announcement = new KalturaAnnouncementService($this);
 		$this->appToken = new KalturaAppTokenService($this);
@@ -14696,6 +14826,7 @@ class KalturaClient extends KalturaClientBase
 		$this->mediaFileType = new KalturaMediaFileTypeService($this);
 		$this->messageTemplate = new KalturaMessageTemplateService($this);
 		$this->meta = new KalturaMetaService($this);
+		$this->mfaPartnerConfiguration = new KalturaMfaPartnerConfigurationService($this);
 		$this->notification = new KalturaNotificationService($this);
 		$this->notificationsPartnerSettings = new KalturaNotificationsPartnerSettingsService($this);
 		$this->notificationsSettings = new KalturaNotificationsSettingsService($this);
