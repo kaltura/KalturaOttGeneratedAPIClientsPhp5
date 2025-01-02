c9088595d2dfa253d20fbad373a48b6a2340082b
Backend CI <ott.rnd.core@kaltura.com>
2025-01-02 11:27:15 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1393, branch 10.8.0.0

--- KalturaEnums.php
+++ KalturaEnums.php
@@ -1271,7 +1271,6 @@ class KalturaImageStatus extends KalturaEnumBase
 {
 	const PENDING = "PENDING";
 	const READY = "READY";
-	const FAILED = "FAILED";
 }
 
 /**
@@ -2511,6 +2510,16 @@ class KalturaTimeShiftedTvState extends KalturaEnumBase
  * @package Kaltura
  * @subpackage Client
  */
+class KalturaTokenDeliveryMethod extends KalturaEnumBase
+{
+	const EMAIL = "Email";
+	const SSO = "Sso";
+}
+
+/**
+ * @package Kaltura
+ * @subpackage Client
+ */
 class KalturaTopicAutomaticIssueNotification extends KalturaEnumBase
 {
 	const INHERIT = "Inherit";
@@ -2731,6 +2740,15 @@ class KalturaVodIngestAssetResultStatus extends KalturaEnumBase
 	const EXTERNAL_FAILURE = "EXTERNAL_FAILURE";
 	const SUCCESS_WARNING = "SUCCESS_WARNING";
 	const SUCCESS = "SUCCESS";
+}
+
+/**
+ * @package Kaltura
+ * @subpackage Client
+ */
+class KalturaWatchBasedRecommendationsProfileOrderBy extends KalturaEnumBase
+{
+	const NONE = "NONE";
 }
 
 /**
