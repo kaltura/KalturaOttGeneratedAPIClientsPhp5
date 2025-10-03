7e4fde328332d4ce76d9a2d2c67a8340dca0ab78
Backend CI <ott.rnd.core@kaltura.com>
2025-10-03 15:06:54 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1559, branch 11.7.0.0

--- KalturaClient.php
+++ KalturaClient.php
@@ -1072,7 +1072,7 @@ class KalturaAssetFilePpvService extends KalturaServiceBase
 	}
 
 	/**
-	 * Update assetFilePpv
+	 * Update assetFilePpv dates
 	 * 
 	 * @param bigint $assetFileId Asset file id
 	 * @param bigint $ppvModuleId Ppv module id
@@ -15584,8 +15584,8 @@ class KalturaClient extends KalturaClientBase
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:25-08-06');
-		$this->setApiVersion('11.5.0.0');
+		$this->setClientTag('php5:25-10-03');
+		$this->setApiVersion('11.7.0.0');
 		
 		$this->aiMetadataGenerator = new KalturaAiMetadataGeneratorService($this);
 		$this->aiRecommendationTree = new KalturaAiRecommendationTreeService($this);
